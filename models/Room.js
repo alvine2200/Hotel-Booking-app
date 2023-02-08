3d69c038d979d440acfd7758c7beade1d2e69e54
alvine2200 <alvinellavu@gmail.com>
2023-02-08 14:14:31 +0300

    room model

---
+++ models/Room.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const RoomSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    maxPeople: {
+      type: Number,
+      required: true,
+    },
+    roomNumber: {
+      type: [{ number: Number, unavailableDates: { type: [Date] } }],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Hotel", HotelSchema);
